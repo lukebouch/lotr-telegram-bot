f803e14a61487fc73e7c6237dcd6a4df1a47f775
Luke Bouch <lukebouch2014@gmail.com>
2021-07-20 20:41:15 -0400

    basic quote bot working

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const config = require('config');
-const https = require('https');
-const { Telegraf } = require('telegraf');
-
-const options = {
-    hostname: config.url,
-    port: 443,
-    path: '/quote',
-    method: 'GET'
-}
+const config = require("config");
+const fetch = require("node-fetch");
+const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(config.telegram);
 
-bot.command('quote', ctx => {
-    let req = https.request(options, res => {
-        res.on('data', d => {
-            bot.telegram.sendMessage(ctx.chat.id, d);
+bot.command("quote", (ctx) => {
+    fetch("https://the-one-api.dev/v2/quote", {
+        headers: {
+            Authorization: `Bearer ${config.token}`,
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            let items = data.docs;
+            let quote = items[Math.floor(Math.random()*items.length)];
+            bot.telegram.sendMessage(ctx.chat.id, quote.dialog);
         });
-    });
 });
 
 bot.launch();
