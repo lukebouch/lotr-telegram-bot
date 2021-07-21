c831a67ee7895f6a73e4e87ce88a8cc7aeacf3b0
Luke Bouch <lukebouch2014@gmail.com>
2021-07-20 20:58:38 -0400

    improves speed and efficiency

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf(config.telegram);
 
 bot.command("quote", (ctx) => {
-    fetch("https://the-one-api.dev/v2/quote", {
+    let random = Math.floor(Math.random()*2390);
+    fetch(`https://the-one-api.dev/v2/quote?limit=1&offset=${random}`, {
         headers: {
             Authorization: `Bearer ${config.token}`,
         },
     })
         .then((response) => response.json())
         .then((data) => {
-            let items = data.docs;
-            let quote = items[Math.floor(Math.random()*items.length)];
-            bot.telegram.sendMessage(ctx.chat.id, quote.dialog);
+            bot.telegram.sendMessage(ctx.chat.id, data['docs'][0]['dialog']);
         });
 });
 
